35b8edfce2b16ee3230e6c9208c1ab0a3007893d
ragreon809@gmail.com <ragreon809@gmail.com>
2024-08-16 17:54:36 +0545

    added includes

---
+++ includes/http_c.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+struct http_c {
+    int PORT;
+    int BUFFER_SIZE;
+};
